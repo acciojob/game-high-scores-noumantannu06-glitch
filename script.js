33a44a90466d8e246664f9daf8541884545220a2
Md Nouman <noumantannu06@gmail.com>
2026-05-26 18:46:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,30 @@ const scores = document.getElementById("scores");
 // Save score to Local Storage
 function saveScore() {
   // complete the code here
+	 const name = nameInput.value.trim();
+  const score = scoreInput.value.trim();
+
+  if (name === "" || score === "") return;
+
+  const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+  savedScores.push({ name, score });
+
+  localStorage.setItem("scores", JSON.stringify(savedScores));
+
+  nameInput.value = "";
+  scoreInput.value = "";
   showScores();
 }
 
 // Show scores in div
 function showScores() {
   // complete the code
+	const savedScores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  scores.innerHTML = savedScores
+    .map((item) => `<p>${item.name}: ${item.score}</p>`)
+    .join("");
 }
+
+
+showScores();
